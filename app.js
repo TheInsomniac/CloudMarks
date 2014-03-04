3e66444777afb89d13b95449ae5a510d862bd7d4
TheInsomniac <manxam2k@hotmail.com>
2014-03-03 22:47:48 -0700

    Added Socket.IO for immediate updates w/o browser refresh

--- app.js
+++ app.js
@@ -1,115 +1,158 @@
 var Datastore = require('nedb'),
-    db = new Datastore({
-      filename: __dirname + '/bookmarks.db',
-      autoload: true
-    });
+  db = new Datastore({
+    filename: __dirname + '/bookmarks.db',
+    autoload: true
+  });
 
 // Auto compact database every 24 hours
 var compactInterval = 3600000 * 24; // milliseconds in hours * num hours
 db.persistence.setAutocompactionInterval(compactInterval);
 
 var express = require('express'),
-    app     = new express();
+  app = new express(),
+  server = require('http').createServer(app),
+  io = require('socket.io').listen(server),
+  request = require('request');
 
-var request = require('request');
+/* Reduce Socket.IO Logging */
+io.set('log level', 1);
 
 app.set('view engine', 'ejs');
-app.set('view options', {layout:false});
+app.set('view options', {
+  layout: false
+});
+
+/* App Title for Index Page */
 app.locals.title = 'CloudMarks';
 
 /* If using Apache or NGINX simple http auth and need a way to log out
-   A "Logout" button will appear on the index page                     */
-app.locals.httpAuth = true;
+   A "Logout" button will appear on the index page if 'true' */
+app.locals.httpAuth = false;
 
-app.use(express.favicon(__dirname + '/favicon.ico', { maxAge: 6000000 }));
-app.use(express.static(__dirname + '/static', { maxAge: 6000000 }));
+app.use(express.favicon(__dirname + '/favicon.ico', {
+  maxAge: 6000000
+}));
+app.use(express.static(__dirname + '/static', {
+  maxAge: 6000000
+}));
 
 /* Truncate string at specified length or closest word boundary + ...
    usage: mystring.trunc(78,true); */
 String.prototype.trunc =
-  function(n,useWordBoundary){
-    var tooLong = this.length>n,
-        s_ = tooLong ? this.substr(0,n-1) : this;
-    s_ = useWordBoundary && tooLong ? s_.substr(0,s_.lastIndexOf(' ')) : s_;
-    return  tooLong ? s_ + '&hellip;' : s_;
-  };
+  function (n, useWordBoundary) {
+    var tooLong = this.length > n,
+      s_ = tooLong ? this.substr(0, n - 1) : this;
+    s_ = useWordBoundary && tooLong ? s_.substr(0, s_.lastIndexOf(' ')) : s_;
+    return tooLong ? s_ + '&hellip;' : s_;
+};
 
-app.get('/clear', function(req, res) {
-  db.remove({}, {multi: true});
-  res.send('Cleared');
-});
-
-app.get('/remove', function(req, res) {
-  var item = req.query.item;
-  removeFromDB(item);
-  res.end();
-});
-
-app.get('/edit', function(req, res) {
-  var item = req.query.item,
-      title = req.query.title;
-  editItem(item, title);
-  res.end();
-});
-
-app.get('/*', function(req, res) {
-  var urlList = [],
-      url = req.originalUrl.slice(1);
-
+/* Add page to Database if in format :
+ * http://Cloudmarks.net/http://sitetoadd.com
+ * Else display index page */
+app.get('/*', function (req, res) {
+  var url = req.originalUrl.slice(1);
   if (url.length) {
-    fetchPageTitle(url, function() {
+    fetchPageTitle(url, function () {
       var pageTitle = this;
-      res.render('added.ejs', {url:url, pageTitle:pageTitle});
+      res.render('added.ejs', {
+        url: url,
+        pageTitle: pageTitle
+      });
     });
   } else {
-    db.find({}, function(err, urlList) {
-      if (err) {
-        res.send('<h3>An Error has occurred!</h3>');
-      } else {
-        urlList.sort(function(a,b) {
-          var c = new Date(a.date),
-              d = new Date(b.date);
-          return d-c;
-        });
-        res.render('index.ejs', {urlList:urlList});
-      }
-    });
+    res.render('index.ejs');
   }
 });
 
-function addToDB(url, pageTitle) {
- var d = new Date();
- d = d.toString();
- db.insert({date: d, url:url, title:pageTitle});
+/* Add new items to database and emit new item to page */
+function addItem(url, pageTitle) {
+  var d = new Date();
+  d = d.toString();
+  var data = {
+    date: d,
+    url: url,
+    title: pageTitle
+  };
+  db.insert(data);
+  io.sockets.emit('added', data);
 }
 
-function removeFromDB(item) {
-  db.remove({_id:item}, {});
+/* Remove item from database and emit remove item from page */
+function removeItem(item) {
+  db.remove({
+    _id: item
+  }, {});
+  io.sockets.emit('removed', item);
 }
 
+/* Edit Page title in Database and emit change to page */
 function editItem(item, title) {
-  db.update({_id:item}, {$set:{title:title}}, {});
+  db.update({
+    _id: item
+  }, {
+    $set: {
+      title: title
+    }
+  }, {});
+  io.sockets.emit('edited', {
+    item: item,
+    title: title
+  });
 }
 
+/* Clear All entries in Database and emit clear page */
+function clearAll() {
+  db.remove({}, {
+    multi: true
+  });
+  io.sockets.emit('cleared', null);
+}
+
+/* Fetch page title on add. If not available, return URL
+ * Truncates Title to 77 characters + elipses */
 function fetchPageTitle(url, callback) {
   var re = /(<\s*title[^>]*>(.+?)<\s*\/\s*title)>/g;
 
-  request(url, function(err, res, body) {
+  request(url, function (err, res, body) {
     if (err) {
       callback.call(null);
     }
     var match = re.exec(body);
     if (match && match[2]) {
       if (match[2].length > 81) {
-        match[2] = match[2].trunc(77,true);
+        match[2] = match[2].trunc(77, true);
       }
-      addToDB(url, match[2]);
+      addItem(url, match[2]);
       callback.call(match[2]);
     } else {
-      addToDB(url, url);
+      addItem(url, url);
       callback.call(url);
     }
   });
 }
 
-app.listen(3000);
+/* Socket.IO Functions */
+io.sockets.on('connection', function (socket) {
+  db.find({}, function (err, urlList) {
+    urlList.sort(function (a, b) {
+      var c = new Date(a.date),
+        d = new Date(b.date);
+      return d - c;
+    });
+    socket.emit('init', urlList);
+  });
+  socket.on('removeItem', function(id) {
+    removeItem(id);
+  });
+  socket.on('editItem', function(data) {
+    var item = data.item;
+    var title = data.title;
+    editItem(item, title);
+  });
+  socket.on('clearAll', function() {
+    clearAll();
+  });
+});
+
+/* Start server on port 3000 */
+server.listen(3000);
---
+++ static/js/socket.js
@@ -0,0 +1,80 @@
+/* Change this to the URL for your server if not just running locally */
+var socket = io.connect('http://localhost');
+
+/* On Reconnect remove old container */
+socket.on('reconnect', function () {
+  console.log('Socket Reconnecting...');
+  var container = document.querySelector('.container');
+  container.remove();
+});
+
+/* On Load build all items, edit and remove buttons */
+socket.on('init', function (data) {
+  console.log('Socket Connected...');
+  var container = document.createElement('div');
+  container.setAttribute('class', 'container');
+  data.forEach(function (url) {
+    var item = document.createElement('div');
+    item.setAttribute('class', 'item');
+    var remove = document.createElement('div');
+    remove.setAttribute('class', 'remove');
+    remove.addEventListener('click', removeItem, false);
+    item.appendChild(remove);
+    var edit = document.createElement('div');
+    edit.setAttribute('class', 'edit');
+    edit.addEventListener('click', editItem, false);
+    item.appendChild(edit);
+    var link = document.createElement('a');
+    link.href = url.url;
+    link.target = '_blank';
+    link.name = url._id;
+    link.id = url._id;
+    link.innerHTML = unescape(url.title);
+    item.appendChild(link);
+    //container.insertBefore(item, document.querySelector('.clear-all'));
+    container.appendChild(item);
+  });
+  document.body.insertBefore(container, document.querySelector('footer'));
+});
+
+/* On Add Item */
+socket.on('added', function (url) {
+  var container = document.querySelector('.container');
+  var item = document.createElement('div');
+  item.setAttribute('class', 'item');
+  var remove = document.createElement('div');
+  remove.setAttribute('class', 'remove');
+  remove.addEventListener('click', removeItem, false);
+  item.appendChild(remove);
+  var edit = document.createElement('div');
+  edit.setAttribute('class', 'edit');
+  edit.addEventListener('click', editItem, false);
+  item.appendChild(edit);
+  var link = document.createElement('a');
+  link.href = url.url;
+  link.target = '_blank';
+  link.name = url._id;
+  link.id = url._id;
+  link.innerHTML = unescape(url.title);
+  item.appendChild(link);
+  container.insertBefore(item, container.childNodes[0]);
+});
+
+/* On Remove Item */
+socket.on('removed', function (id) {
+  var item = document.getElementById(id);
+  item.parentElement.remove();
+});
+
+/* On Edit Item */
+socket.on('edited', function (data) {
+  var item = document.getElementById(data.item);
+  item.innerHTML = data.title;
+});
+
+/* On Clear All */
+socket.on('cleared', function () {
+  var container = document.querySelector('.container');
+  container.remove();
+});
+
